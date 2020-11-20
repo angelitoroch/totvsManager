04d0bcc38462a1c1acbbcc63848513c780dffda3
angelito_roch <akira_aar@hotmail.es>
2020-11-19 18:47:43 -0600

    Se corrigio el error del buscador y se anexo la visualizacion grafica de la seccion de modificar tarea

--- src/app/modules/tasks/components/modify-task/modify-task.component.ts
+++ src/app/modules/tasks/components/modify-task/modify-task.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PoModalComponent, PoSelectOption } from '@po-ui/ng-components';
+import { Category } from 'src/app/modules/category/interfaces/category';
+import { CategoryService } from 'src/app/modules/category/services/category.service';
+import { Task } from '../../interfaces/task';
+import { TaskService } from '../../service/task.service';
 
 @Component({
   selector: 'app-modify-task',
@@ -6,7 +11,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modify-task.component.css'],
 })
 export class ModifyTaskComponent implements OnInit {
-  constructor() {}
+  tasks: Task[]; //Arreglo donde se guardaran los valores de todos las taareas
+  taskPadre: Task; //Variable del tipo Task que servira para pasar objeto a un componente Hijo
+  typeCategory: number = 0; //Tategoria por default
+  categorys: Category[]; //Variable donde se guardaran las categorias
+  @ViewChild(PoModalComponent, { static: true }) poModal: PoModalComponent;
 
-  ngOnInit(): void {}
+  //Array con las opciones definidas para las categorias
+  typesCategorys: Array<PoSelectOption> = [
+    { label: 'TODAS LAS CATEGORIAS', value: 0 },
+  ];
+
+  constructor(
+    private taskService: TaskService,
+    private categoryService: CategoryService
+  ) {}
+
+  ngOnInit() {
+    this.getCategorys();
+    this.getTasks();
+  }
+
+  //Metodo que abre el Modal y pasa el objeto task del componente padre al componente hijo
+  openModalDetails(oTask: Task) {
+    this.taskPadre = oTask;
+    this.poModal.open();
+  }
+
+  //Metodo que traera todas las tareas usando el servicio con HTTP
+  getTasks() {
+    this.taskService.getTasks().subscribe(
+      (tasksFromApi: Task[]) => {
+        this.tasks = tasksFromApi;
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  //Metodo que traera todas las categorias usando el servicio con HTTP
+  getCategorys() {
+    this.categoryService.getCategorys().subscribe(
+      (categorysFromApi: Category[]) => {
+        this.categorys = categorysFromApi;
+        this.joinCategorys();
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  //Metodo que obtiene las categorias y las agrega al arreglo typeCategorys
+  joinCategorys() {
+    for (let category of this.categorys) {
+      this.typesCategorys.push({
+        label: category.nombre,
+        value: category.id,
+      });
+    }
+  }
+
+  //Metodo que se dispara cuando se cambia la opcion en las opciones de selecciona tu categotria
+  changeCategoryId(id: number) {
+    if (id != 0) {
+      this.taskService.getTasksId(id).subscribe(
+        (tasksFromApi: Task[]) => {
+          console.log(tasksFromApi);
+          this.tasks = tasksFromApi;
+        },
+        (error) => console.error(error)
+      );
+    } else {
+      this.getTasks();
+    }
+  }
 }
