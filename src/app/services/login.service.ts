7ece68e136feaf5fd4cd8c2d93e67be0392d7d87
Luis Aboytes <akira_aar@hotmail.es>
2020-11-25 07:28:10 -0600

    Se implemento la funcionalidad de mantener la sesión abierta ubicando archivos o datos en localstorage

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,20 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Users } from '../interfaces/users';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
   //Se definen las rutas de la api
-  baseURL = environment.apiURL + '/users/';
+  baseURL = environment.apiURL + '/users/';//Busca el usuario y sus datos
+  baseURL2 = environment.apiURL + '/users?correo=';//Busca por correo para el login
 
-  constructor(private http: HttpClient) { }
+  //Se inicializa el servicio
+  constructor(private http: HttpClient) {}
 
   //Metodo que recibe los datos ingresados en el formulario
-  getUser(user:Users){
-    
+  getUser(correo: string): Observable<Users> {
+    const url = this.baseURL2 + correo;
+    return this.http.get<Users>(url);
   }
 }
